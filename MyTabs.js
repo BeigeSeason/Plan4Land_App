1f2b3c4b9a1593a58e285e9832387201ff44a564
BeigeSeason <kima606@gmail.com>
2025-04-24 11:25:07 +0900

    앱 이름 변경 및 상단 마진 제거

--- MyTabs.js
+++ MyTabs.js
@@ -274,7 +274,7 @@ function Planning({ navigation, route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+    // marginTop: StatusBar.currentHeight || 0,
   },
   tabBarStyle: {
     ...Platform.select({
